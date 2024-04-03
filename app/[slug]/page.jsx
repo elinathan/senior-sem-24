54a6804556aeb25cf2af593b1b8fee862679969a
eli <enathan@seas.upenn.edu>
2024-04-02 20:33:29 -0400

    generate static params

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,15 +1,28 @@
-import { fetchPageBlocks, fetchPageBySlug, notion } from "@/lib/notion";
+import {
+  fetchPageBlocks,
+  fetchPageBySlug,
+  fetchPages,
+  notion,
+} from "@/lib/notion";
 import bookmarkPlugin from "@notion-render/bookmark-plugin";
 import { NotionRenderer } from "@notion-render/client";
 import hljsPlugin from "@notion-render/hljs-plugin";
 import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const posts = await fetchPages().then((res) => res.results);
+
+  return posts.map((post) => ({
+    slug: post.properties.Slug.rich_text[0].plain_text,
+  }));
+}
 
 export default async function Page({ params }) {
   const post = await fetchPageBySlug(params.slug);
   if (!post) notFound();
 
   const blocks = await fetchPageBlocks(post.id);
-  console.log(blocks);
+  // console.log(blocks);
 
   const renderer = new NotionRenderer({
     client: notion,
