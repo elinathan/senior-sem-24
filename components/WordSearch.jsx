9e79cccda12c49e6a8565b71b7aaa0708dac2b16
eli <enathan@seas.upenn.edu>
2024-04-02 21:38:20 -0400

    alignment

--- components/WordSearch.jsx
+++ components/WordSearch.jsx
@@ -3,11 +3,11 @@
 export default function WordSearch({ fullText }) {
   const characters = Array.from(fullText);
   return (
-    <div className="grid-cols-15 mx-auto grid w-max gap-2 md:gap-3">
+    <div className="grid-cols-15 mx-auto grid w-max gap-1 md:gap-3">
       {characters.map((letter, index) => {
         return (
           <span
-            className=" size-[14px] text-center text-lg font-semibold leading-none md:size-6 md:text-xl"
+            className=" flex size-[18px] justify-center text-center text-lg font-semibold leading-none md:size-6 md:text-xl"
             key={index}
           >
             {letter}
