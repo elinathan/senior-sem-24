10979c405698c42e4fa82eb20d15026e0f645081
eli <enathan@seas.upenn.edu>
2024-04-16 21:36:25 -0400

    format student info

--- components/StudentInfo.jsx
+++ components/StudentInfo.jsx
@@ -1,15 +1,30 @@
+import Link from "next/link";
+
 export default function StudentInfo({ student }) {
   return (
-    <div className="mx-auto flex max-w-2xl justify-between">
-      <div className="flex flex-col gap-2">
+    <div className="mx-auto flex max-w-2xl flex-col justify-between md:flex-row">
+      <div className=" flex flex-col gap-2 md:w-1/2">
         <p className="p-0">{student.category}</p>
         <p className="p-0">{student.medium}</p>
         <p className="p-0">{student.themes}</p>
       </div>
-      <div>
-        <p className="">{student.socialMediaHandles}</p>
-        <p className="">{student.portfolioSite}</p>
-        <p className="">{student.linkedin}</p>
+      <div className="flex flex-col gap-2 md:w-1/3 md:text-right">
+        <p className="p-0">{student.socialMediaHandles}</p>
+
+        <Link
+          href={`https://${student.portfolioSite}`}
+          target="_blank"
+          className="p-0"
+        >
+          {student.portfolioSite}
+        </Link>
+        <Link
+          href={`https://${student.linkedin}`}
+          className="p-0"
+          target="_blank"
+        >
+          LinkedIn
+        </Link>
       </div>
     </div>
   );
